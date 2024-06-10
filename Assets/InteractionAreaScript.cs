1a013defa1901a7338c7fa0fff816f6b88aa81c1
UriZaltzman <144272394+UriZaltzman@users.noreply.github.com>
2024-06-10 16:42:55 -0300

    Initial commit

---
+++ Assets/InteractionAreaScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionAreaScript : MonoBehaviour
+{
+    public int puntos = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void puntoss(int puntsiaems)
+    {
+        puntos += puntsiaems;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PickUpScript pickUp;
+        pickUp = GetComponent<PickUpScript>();
+        if (pickUp.esComida == true)
+        {
+            puntoss(pickUp.puntosItem);
+            Destroy(other.gameObject);
+
+        }
+        else
+        {
+            puntoss(pickUp.puntosItem);
+        }
+    }
+}
